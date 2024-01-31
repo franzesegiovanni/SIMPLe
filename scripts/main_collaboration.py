dfe2c5c607b5aabd9503ceac5f0f82e712c97831
franzesegiovanni <giovanni.franzese94@gmail.com>
2024-01-31 20:01:38 +0100

    created scripts for linda celebration day

---
+++ scripts/main_collaboration.py
@@ -0,0 +1,36 @@
+"""
+Authors: Giovanni Franzese 
+Email: g.franzese@tudelft.nl
+Cognitive Robotics, TU Delft
+This code is part of TERI (TEaching Robots Interactively) project
+"""
+#%%
+from SIMPLe_bimanual.dual_panda import DualPanda
+import time
+#%%
+if __name__ == '__main__':
+    BiManualTeaching=DualPanda()
+    time.sleep(1)
+    #%%
+    BiManualTeaching.Kinesthetic_Demonstration_BiManual()
+    #%%
+    BiManualTeaching.save(data='collaboration')
+    #%%
+    BiManualTeaching.load(data='collaboration')
+    # %%
+    BiManualTeaching.go_to_start()
+    #%%
+    BiManualTeaching.execute_dual()
+    # %%
+    BiManualTeaching.Panda_left.set_stiffness(0.0, 0.0, 0.0, 30.0, 30.0, 30.0)
+    BiManualTeaching.Panda_right.set_stiffness(0.0, 0.0, 0.0, 30.0, 30.0, 30.0)
+    BiManualTeaching.Panda_left.set_K.update_configuration({"coupling_translational_stiffness": 400})
+    # %% Close
+    BiManualTeaching.Panda_left.move_gripper(0.0)
+    BiManualTeaching.Panda_right.move_gripper(0.0)
+    #%%
+    # %% Open 
+    BiManualTeaching.Panda_left.move_gripper(1.0)
+    BiManualTeaching.Panda_right.move_gripper(1.0)
+
+# %%
